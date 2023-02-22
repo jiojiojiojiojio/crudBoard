0fd06969acdbb86b22fdf8696dc67a4b0e65ac5d
jio <kwldh99@naver.com>
2023-02-23 03:03:57 +0900

    마이페이지 - 예전에 넣었던 임의 값 없애고 u_id session값 불러오기

--- src/main/java/com/jiocompany/crudBoard/controller/MyPageController.java
+++ src/main/java/com/jiocompany/crudBoard/controller/MyPageController.java
@@ -1,34 +1,62 @@
 package com.jiocompany.crudBoard.controller;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.jiocompany.crudBoard.mypage.MypageDTO;
+import com.jiocompany.crudBoard.mypage.MypageService;
 
 @Controller
 public class MyPageController {
 	private static final Logger logger = LoggerFactory.getLogger(MyPageController.class);
 
+	@Autowired 
+	MypageService service;
+	
 	//마이페이지 내 정보 조회
 	@RequestMapping(value="/mypage", method = RequestMethod.GET)
-	public String mypage() {
+	public ModelAndView mypage(HttpSession session) throws Exception{
+		logger.info("내 정보 조회");
 		
-		return "mypage";
-	}
-	
-	//마이페이지 내 정보 수정
-	@RequestMapping(value="mypage/user_info", method = RequestMethod.GET)
-	public String user_info() {
+		ModelAndView mav = new ModelAndView();
 		
-		return "user_info";
+		String u_id=(String) session.getAttribute("u_id");
+		
+		logger.info("session에 저장된 uid>>>>"+u_id);
+		
+		MypageDTO mypageDTO = service.mypage(u_id);
+		
+		mav.addObject("mypageDTO", mypageDTO);
+		mav.setViewName("mypage");
+		return mav;
 	}
 	
 	//마이페이지 내 정보 수정 저장
 	@RequestMapping(value="mypage/user_info", method = RequestMethod.POST)
-	public String user_info_post() {
+	public String user_info(MypageDTO mypageDTO , HttpServletRequest request) throws Exception {
+		logger.info("내 정보 수정 저장 이동");
+		request.setCharacterEncoding("utf-8");
+		int r = service.user_info(mypageDTO);
 		
+		//수정 성공시
+		if(r>0) {
+			return "redirect:/mypage?u_id="+mypageDTO.getU_id();
+		}
+		
+		//수정 실패시
+		logger.info("수정 실패ㅠㅠㅠ");
 		return "user_info";
 	}
 	
